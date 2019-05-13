bcdd6ee2555879277ed30400171e1b92e93754ba
Miguel Chavez <miguelchavez@Miguels-MacBook-Pro.local>
2019-05-13 12:48:23 -0400

    Update npm packages and refernces

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAM07eoJunDu4jNENR9D3jlkIU72dF38_8",
+    authDomain: "miguelchavezweb.firebaseapp.com",
+    databaseURL: "https://miguelchavezweb.firebaseio.com",
+    projectId: "miguelchavezweb",
+    storageBucket: "miguelchavezweb.appspot.com",
+    messagingSenderId: "22195742987"
+  }
 };
